408814f41f6def9dabbe60052a0f52e24cdc0156
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-03-10 12:26:11 +0000

    Upload user image

--- lynx/forms.py
+++ lynx/forms.py
@@ -4,6 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth.models import User
 
+from . models import Profile
+
 # user log-in
 
 from django.contrib.auth.forms import AuthenticationForm
@@ -26,6 +28,31 @@ class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=TextInput())
     password = forms.CharField(widget=PasswordInput())
     
+
+
+class UpdateUserForm(forms.ModelForm):
+
+    password = None
+
+    class Meta:
+
+        model = User
+
+        fields = ['username', 'email']
+        exclude = ['password1', 'password2']
+
+
+# Updating our profile picture
+
+class UpdateProfileForm(forms.ModelForm):
+
+    profile_pic = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+
+    class Meta:
+
+        model = Profile
+        fields = ['profile_pic']
+
 
 
 
